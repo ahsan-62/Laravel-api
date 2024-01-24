eb2125b67f508b0a074c62462999d1b7321e03d6
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-01-24 22:23:48 +0600

    Single User POST API

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserApiController extends Controller
 {
@@ -16,5 +17,47 @@ class UserApiController extends Controller
             $users=User::find($id);
            return response()->json(['users'=>$users],200);
         }
+    }
+
+
+    public function createUser(Request $request) {
+
+
+        if($request->isMethod('post')){
+
+            $data=$request->all();
+
+            $rules=[
+
+                'name'=>'required',
+                'email'=>'required|email|unique:users',
+                'password'=>'required',
+            ];
+
+            $customMessage=[
+                'name.required'=>'Name is required',
+                'email.required'=>'Email is required',
+                'email.email'=>'Email must be valid',
+                'email.unique'=>'Email already exists',
+                'password.required'=>'Password is required',
+            ];
+
+            $validator= Validator::make($data,$rules,$customMessage);
+            if($validator->fails()){
+                return response()->json(['error'=>$validator->errors()],422);
+            }
+
+
+            $user= new User();
+            $user->name=$data['name'];
+            $user->email=$data['email'];
+            $user->password=$data['password'];
+            $user->save();
+
+            $message='User created successfully';
+            return response()->json(['message'=>$message],200);
+
+        }
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/users/{id?}',[UserApiController::class,'showUsers']);
+Route::post('/create-user',[UserApiController::class,'createUser']);
